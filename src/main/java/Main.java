1ff4c85b85375c711d310ee5e3fb0fc3fe1423f1
krechmerea.24 <egor_vologda2006@mail.ru>
2022-11-24 13:19:16 +0300

    java doc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 import app.Application;
 import io.github.humbleui.jwm.App;
 
+/**
+ * главный класс
+ */
 public class Main {
-
+    /**
+     * тело главного класса
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         App.start(Application::new);
     }
--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,16 +4,28 @@ import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
+/**
+ * класс окна приложения
+ */
 public class Application implements Consumer<Event> {
-
+    /**
+     * окно приложения
+     */
     private final Window window;
 
+    /**
+     * конструктор класса приложение
+     */
     public Application() {
         window = App.makeWindow();
         window.setEventListener(this);
         window.setVisible(true);
     }
 
+    /**
+     * Обработчик событий
+     * @param e the input argument
+     */
     @Override
     public void accept(Event e) {
         if (e instanceof EventWindowClose) {
