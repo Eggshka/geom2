ba3b4231fae8328eff6a751374719bc521e28d86
krechmerea.24 <egor_vologda2006@mail.ru>
2022-12-22 12:08:52 +0300

    4 panels

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -9,6 +9,10 @@ import io.github.humbleui.skija.RRect;
 import io.github.humbleui.skija.Surface;
 import misc.CoordinateSystem2i;
 import misc.Misc;
+import panels.PanelControl;
+import panels.PanelHelp;
+import panels.PanelLog;
+import panels.PanelRendering;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -24,6 +28,22 @@ public class Application implements Consumer<Event> {
  * радиус скругления элементов
  */
 public static final int C_RAD_IN_PX = 4;
+    /**
+     * панель легенды
+     */
+    private final PanelHelp panelHelp;
+    /**
+     * панель курсора мыши
+     */
+    private final PanelControl panelControl;
+    /**
+     * панель рисования
+     */
+    private final PanelRendering panelRendering;
+    /**
+     * панель событий
+     */
+    private final PanelLog panelLog;
     /**
      * Первый заголовок
      */
@@ -73,6 +93,26 @@ public static final int C_RAD_IN_PX = 4;
                 System.out.println("Ошибка создания слоя " + className);
             }
         }
+        // создаём панель рисования
+        panelRendering = new PanelRendering(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 0, 0,
+                3, 2
+        );
+        // создаём панель управления
+        panelControl = new PanelControl(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 3, 0,
+                2, 2
+        );
+        // создаём панель лога
+        panelLog = new PanelLog(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 0, 2,
+                3, 1
+        );
+        // создаём панель помощи
+        panelHelp = new PanelHelp(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 3, 2,
+                2, 1
+        );
 
         label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
                 4, 4, 1, 1, 1, 1, "Привет, мир!", true, true);
@@ -111,11 +151,15 @@ public static final int C_RAD_IN_PX = 4;
      * @param windowCS СК окна
      */
     public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
+        // запоминаем изменения (пока что там просто заливка цветом)
         canvas.save();
+        // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-        label.paint(canvas,windowCS);
-        label2.paint(canvas, windowCS);
-        label3.paint(canvas, windowCS);
+        // рисуем панели
+        panelRendering.paint(canvas, windowCS);
+        panelControl.paint(canvas, windowCS);
+        panelLog.paint(canvas, windowCS);
+        panelHelp.paint(canvas, windowCS);
         canvas.restore();
     }
 }
