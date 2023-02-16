a945d8c77768ec28fc70b1224a8535780fd83425
krechmerea.24 <egor_vologda2006@mail.ru>
2023-02-16 11:11:53 +0300

    cs rendering

--- src/main/java/misc/CoordinateSystem2i.java
+++ src/main/java/misc/CoordinateSystem2i.java
@@ -147,6 +147,33 @@ public class CoordinateSystem2i {
     public RRect getRRect(float rad) {
         return RRect.makeXYWH(min.x, min.y, size.x, size.y, rad);
     }
+    /**
+     * Получить вектор подобия двух систем координат
+     * (значения единичного размера, указанного в переданнной в аргументах СК в текущей СК)
+     *
+     * @param coordinateSystem система координат, подобие с которой нужно получить
+     * @return вектор подобий вдоль соответствующиъ осей координат
+     */
+    public Vector2i getSimilarity(CoordinateSystem2d coordinateSystem) {
+        return new Vector2i(
+                (int) ((size.x - 1) / (coordinateSystem.getSize().x)),
+                (int) ((size.y - 1) / (coordinateSystem.getSize().y))
+        );
+    }
+
+    /**
+     * Получить вектор подобия двух систем координат
+     * (значения единичного размера, указанного в переданнной в аргументах СК в текущей СК)
+     *
+     * @param coordinateSystem система координат, подобие с которой нужно получить
+     * @return вектор подобий вдоль соответствующиъ осей координат
+     */
+    public Vector2i getSimilarity(CoordinateSystem2i coordinateSystem) {
+        return new Vector2i(
+                (size.x - 1) / (coordinateSystem.getSize().x - 1),
+                (size.y - 1) / (coordinateSystem.getSize().y - 1)
+        );
+    }
 
     /**
      * Проверить, попадают ли координаты в границы СК
